4dc4c6e00023ec681a0e001d560886e29e5f8ba9
Tejas20002 <tejashirani55@gmail.com>
2023-03-19 19:23:45 +0530

    [*]  create adminlte3 layout for dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,83 +1,48 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('adminlte::master')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@inject('layoutHelper', 'JeroenNoten\LaravelAdminLte\Helpers\LayoutHelper')
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+@section('adminlte_css')
+    @stack('css')
+    @yield('css')
+@stop
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+@section('classes_body', $layoutHelper->makeBodyClasses())
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+@section('body_data', $layoutHelper->makeBodyData())
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+@section('body')
+    <div class="wrapper">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+        {{-- Top Navbar --}}
+        @if($layoutHelper->isLayoutTopnavEnabled())
+            @include('adminlte::partials.navbar.navbar-layout-topnav')
+        @else
+            @include('adminlte::partials.navbar.navbar')
+        @endif
 
-                    </ul>
+        {{-- Left Main Sidebar --}}
+        @if(!$layoutHelper->isLayoutTopnavEnabled())
+            @include('adminlte::partials.sidebar.left-sidebar')
+        @endif
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+        {{-- Content Wrapper --}}
+        {{ $slot }}
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+        {{-- Footer --}}
+        @hasSection('footer')
+            @include('adminlte::partials.footer.footer')
+        @endif
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+        {{-- Right Control Sidebar --}}
+        @if(config('adminlte.right_sidebar'))
+            @include('adminlte::partials.sidebar.right-sidebar')
+        @endif
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
-</body>
-</html>
+@stop
+
+@section('adminlte_js')
+    @stack('js')
+    @yield('js')
+@stop
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->to('login');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/google/callback', [AuthController::class, 'loginWithGoogle'])->name('login-with-google');
-Route::get('/login', [AuthController::class, 'redirectToGoogle'])->name('login-with-google-redirect');
+Route::get('google/login', [AuthController::class, 'redirectToGoogle'])->name('login-with-google-redirect');
