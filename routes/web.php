ca038a357ef9e2679cef98aaa7099762115b424f
Tejas20002 <tejashirani55@gmail.com>
2023-03-19 19:11:26 +0530

    [*] google login complete

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/google/callback', [AuthController::class, 'loginWithGoogle'])->name('login-with-google');
+Route::get('/login', [AuthController::class, 'redirectToGoogle'])->name('login-with-google-redirect');
